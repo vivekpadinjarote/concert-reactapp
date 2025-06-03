c06e335cc550d08f4853aeb90570205dee371390
vivekpadinjarote <85781335+vivekpadinjarote@users.noreply.github.com>
2025-06-03 12:15:21 +0530

    commit-2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { Link } from "react-router-dom";
 import "./App.css";
 import Navbar from "./Components/Navbar";
-import checkAuth from "./Components/auth/checkAuth";
 import HomePage from "./Components/home";
 
 function App() {
